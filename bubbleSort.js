55b70817ca5d318596abc252b840f2b5be17c4ea
Edward Gao <egao@sent.com>
2017-02-01 21:22:59 -1000

    simplify code

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,11 +1,10 @@
 module.exports = function(arr) {
-	let notDone, temp;
-	notDone = true;
+	let notDone = true;
 	while(notDone) {
 		notDone = false;
 		for(let i = 0; i < arr.length - 2; i++) {
 			if(arr[i] > arr[i + 1]) {
-				temp = arr[i + 1];
+				let temp = arr[i + 1];
 				arr[i + 1] = arr[i];
 				arr[i] = temp;
 				notDone = true;
