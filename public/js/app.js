4bba1e5a8fc27d7515719d3080e9d3967ce34296
Edward Gao <egao@sent.com>
2017-02-02 02:34:59 -1000

    vBubble.js actually works perfectly

--- public/js/app.js
+++ public/js/app.js
@@ -1,92 +1,12 @@
-var vBubble = (function() {
-
-	var arr = [];
-	var i = 0;
-	var round = 0;
-	var notDone = true;
-	var disorder = false;
-	var stack = [];
-	var returnObj = {
-		round: 0,
-		action: null,
-		opID: []
+(function initialize() {
+	for(let i = 1; i <= 10; i++) {
+		vBubble.append(document.getElementById(i).innerText);
 	}
-
-	function _append(num) {
-		arr.push(num);
-	}
-
-	function _remove(id) {
-		arr.splice(id, 1);
-	}
-
-	function _loopThru() {
-		if(i < arr.length - 1) {
-			let needSwitch = arr[i] > arr[i+1];
-			returnObj.action = 'compare';
-			returnObj.opID = [arr[i], arr[i+1]];
-			if(needSwitch) {
-				notDone = true;
-				disorder = true;
-			}else{
-				i++;
-			}
-			return returnObj;
-		}else{
-			i = 0;
-			stack.pop();
-			return _next();
-		}
-	}
-
-	function _repeat() {
-		if(notDone) {
-			stack.push(_loopThru);
-			notDone = false;
-			return _next();
-		}else{
-			returnObj.action = 'end';
-			return returnObj;
-		}
-	}
-
-	function _reset() {
-		stack.push(_repeat);
-		stack.push(_loopThru);
-	}
-
-	function _next() {
-		if(disorder) {
-			disorder = false;
-			returnObj.action = 'switch';
-			returnObj.opID = [arr[i], arr[i+1]];
-			let temp = arr[i];
-			arr[i] = arr[i+1];
-			arr[i+1] = temp;
-			i++;
-			return returnObj;
-		}else{
-			return stack[stack.length-1]();
-		}
-	}
-
-	return {
-		append: _append,
-		remove: _remove,
-		reset: _reset,
-		next: _next,
-		arr: arr
-	}
-
+	vBubble.reset();
 })();
 
-for(let i = 1; i <= 10; i++) {
-	vBubble.append(document.getElementById(i).innerText);
-}
-vBubble.reset();
-
-let output = { action: 'start' };
-while(output.action !== 'end') {
-	output = vBubble.next();
+document.getElementById('title').addEventListener('click', () => {
+	let output = vBubble.next();
 	console.log(output.opID[0], output.opID[1]);
-}
+	console.log(vBubble.arr);
+});
---
+++ public/js/vBubble.js
@@ -0,0 +1,81 @@
+var vBubble = (function() {
+
+	var arr = [];
+	var i = 0;
+	var round = 0;
+	var notDone = true;
+	var disorder = false;
+	var stack = [];
+	var returnObj = {
+		round: 0,
+		action: null,
+		opID: []
+	}
+
+	function _append(num) {
+		arr.push(Number(num));
+	}
+
+	function _remove(id) {
+		arr.splice(id, 1);
+	}
+
+	function _loopThru() {
+		if(i < arr.length - 1) {
+			let needSwitch = arr[i] > arr[i+1];
+			returnObj.action = 'compare';
+			returnObj.opID = [arr[i], arr[i+1]];
+			if(needSwitch) {
+				notDone = true;
+				disorder = true;
+			}else{
+				i++;
+			}
+			return returnObj;
+		}else{
+			i = 0;
+			stack.pop();
+			return _next();
+		}
+	}
+
+	function _repeat() {
+		if(notDone) {
+			stack.push(_loopThru);
+			notDone = false;
+			return _next();
+		}else{
+			returnObj.action = 'end';
+			return returnObj;
+		}
+	}
+
+	function _reset() {
+		stack.push(_repeat);
+		stack.push(_loopThru);
+	}
+
+	function _next() {
+		if(disorder) {
+			disorder = false;
+			returnObj.action = 'switch';
+			returnObj.opID = [arr[i], arr[i+1]];
+			let temp = arr[i];
+			arr[i] = arr[i+1];
+			arr[i+1] = temp;
+			i++;
+			return returnObj;
+		}else{
+			return stack[stack.length-1]();
+		}
+	}
+
+	return {
+		append: _append,
+		remove: _remove,
+		reset: _reset,
+		next: _next,
+		arr: arr
+	}
+
+})();
