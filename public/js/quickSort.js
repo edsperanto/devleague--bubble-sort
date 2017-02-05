20462b7e4e3547b6cf61469a32821617b143cea4
Edward Gao <egao@sent.com>
2017-02-04 15:35:30 -1000

    quicksort is working

--- public/js/quickSort.js
+++ public/js/quickSort.js
@@ -1,17 +1,34 @@
 module.exports = function quickSort(arr) {
-	let left, right;
-	let pivot = arr[0];
-	left = right = [];
+	let same = true;
+	let left = [];
+	let right = [];
+	let pivot = [arr[0]];
 
 	arr.forEach(num => {
-		if(num < pivot) {
+		if(pivot[0] == num) {
+			pivot.push(num);
+		}else if(pivot[0] > num) {
 			left.push(num);
 		}else{
 			right.push(num);
 		}
 	});
 
-	left = (left.length > 1) ? quickSort(left) : left;
-	right = (right.length > 1) ? quickSort(right) : right;
-	return left.concat(right);
+	if(pivot.length > 1) {
+		pivot.pop();
+	}
+
+	function swap(arr) {
+		if(arr.length !== 1 && arr.length !== 0) {
+			let tmp = arr[0];
+			arr[0] = arr[1];
+			arr[1] = tmp;
+		}
+		return arr;
+	}
+
+	left = (left.length > 2) ? quickSort(left) : swap(left);
+	right = (right.length > 2) ? quickSort(right) : swap(right);
+
+	return left.concat(pivot.concat(right));
 }
--- test/test-script.js
+++ test/test-script.js
@@ -1,8 +1,9 @@
 const chai = require('chai');
 const should = chai.should();
 const bubbleSort = require('../public/js/bubbleSort');
+const quickSort = require('../public/js/quickSort');
 
-describe('Algorithm', () => {
+describe('Bubble Sort', () => {
 	it('should sort array [5,1,4,2,8]', () => {
 		let test = bubbleSort([5,1,4,2,8]);
 		test.should.deep.equal([1,2,4,5,8]);
@@ -21,7 +22,7 @@ describe('Algorithm', () => {
 	});
 });
 
-describe('Edge cases', () => {
+describe('Bubble Sort edge cases', () => {
 	it('should allow empty array', () => {
 		let test = bubbleSort([]);
 		test.should.deep.equal([]);
@@ -53,3 +54,56 @@ describe('Edge cases', () => {
 		test.should.equal('Expected array, received object');
 	});
 });
+
+describe('Quick Sort', () => {
+	it('should sort array [5,1,4,2,8]', () => {
+		let test = quickSort([5,1,4,2,8]);
+		test.should.deep.equal([1,2,4,5,8]);
+	});
+	it('should sort array [0,0,0,0]', () => {
+		let test = quickSort([0,0,0,0]);
+		test.should.deep.equal([0,0,0,0]);
+	});
+	it('should sort array [8,8,8,6,8]', () => {
+		let test = quickSort([8,8,8,6,8]);
+		test.should.deep.equal([6,8,8,8,8]);
+	});
+	it('should sort array [0.2,3.4,63,0,2.8]', () => {
+		let test = quickSort([0.2,3.4,63,0,2.8]);
+		test.should.deep.equal([0,0.2,2.8,3.4,63]);
+	});
+});
+/*
+describe('Quick Sort edge cases', () => {
+	it('should allow empty array', () => {
+		let test = quickSort([]);
+		test.should.deep.equal([]);
+	});
+	it('should allow array with one number', () => {
+		let test = quickSort([9]);
+		test.should.deep.equal([9]);
+	});
+	it('should reject non-numbers in array', () => {
+		let test = quickSort([5,"four",3,2,1]);
+		test.should.equal('Item \'four\' is string instead of number');
+	});
+	it('should reject strings', () => {
+		let test = quickSort("ayy lmao");
+		test.should.equal('Expected array, received string');
+	});
+	it('should reject numbers', () => {
+		let test = quickSort(666);
+		test.should.equal('Expected array, received number');
+	});
+	it('should reject booleans', () => {
+		let testTrue = quickSort(true);
+		testTrue.should.equal('Expected array, received boolean');
+		let testFalse = quickSort(false);
+		testFalse.should.equal('Expected array, received boolean');
+	});
+	it('should reject objects', () => {
+		let test = quickSort({});
+		test.should.equal('Expected array, received object');
+	});
+});
+*/
