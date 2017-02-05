dfb8d5ab8cc5029269705e8a6c21d226af753af4
Edward Gao <egao@sent.com>
2017-02-04 16:39:33 -1000

    turn into function

--- public/js/quickSort.js
+++ public/js/quickSort.js
@@ -1,8 +1,7 @@
 module.exports = function q(y) {
 	let l = [], r = [], p = [], g = 'length';
+	let f = a => a = (a[g] > 2) ? q(a) : ((a[g] === 2) ? [a[1], a[0]] : a);
 	y.forEach(n => ((y[0] === n) ? p : ((y[0] > n) ? l : r)).push(n));
-	l = (l[g] > 2) ? q(l) : ((l[g] === 2) ? [l[1], l[0]] : l);
-	r = (r[g] > 2) ? q(r) : ((r[g] === 2) ? [r[1], r[0]] : r);
-
+	l = f(l); r = f(r);
 	return l.concat(p.concat(r));
 }
