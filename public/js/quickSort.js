c9e4e45833a4109c4761f480b0b67797c5e03c3f
Edward Gao <egao@sent.com>
2017-02-04 16:31:13 -1000

    one-letterize everything

--- public/js/quickSort.js
+++ public/js/quickSort.js
@@ -1,11 +1,8 @@
-module.exports = function quickSort(arr) {
-	let l = [];
-	let r = [];
-	let p = [];
-
-	arr.forEach(num => ((arr[0] === num) ? p : ((arr[0] > num) ? l : r)).push(num));
-	l = (l.length > 2) ? quickSort(l) : ((l.length === 2) ? [l[1], l[0]] : l);
-	r = (r.length > 2) ? quickSort(r) : ((r.length === 2) ? [r[1], r[0]] : r);
+module.exports = function q(y) {
+	let l = [], r = [], p = [], g = 'length';
+	y.forEach(n => ((y[0] === n) ? p : ((y[0] > n) ? l : r)).push(n));
+	l = (l[g] > 2) ? q(l) : ((l[g] === 2) ? [l[1], l[0]] : l);
+	r = (r[g] > 2) ? q(r) : ((r[g] === 2) ? [r[1], r[0]] : r);
 
 	return l.concat(p.concat(r));
 }
