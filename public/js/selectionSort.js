b1338335e7620dbed97e36aa41b90a4c09298663
Edward Gao <egao@sent.com>
2017-02-06 23:15:41 -1000

    edge cases for selection sort

--- public/js/selectionSort.js
+++ public/js/selectionSort.js
@@ -1,4 +1,12 @@
 module.exports = function selectionSort(arr) {
+	if(arr instanceof Array == false) {
+		return `Expected array, received ${typeof arr}`;
+	}
+	for(let i = 0; i < arr.length; i++) {
+		if(isNaN(Number(arr[i]))) {
+			return `Item '${arr[i]}' is ${typeof arr[i]} instead of number`;
+		}
+	}
 	for(let currIdx = 0; currIdx < arr.length; currIdx++) {
 		let tmp;
 		let smallest = arr[currIdx];
